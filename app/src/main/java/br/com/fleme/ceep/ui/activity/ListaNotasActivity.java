6671d48918d03687d7cefa5c9c010300b1cc4d1f
felleme <felleme@prestadores.amil.com.br>
2018-11-22 17:51:25 -0200

    Criando as classes iniciais do projeto para conseguirmos popular uma lista com dados fixos.

--- app/src/main/java/br/com/fleme/ceep/ui/activity/ListaNotasActivity.java
+++ app/src/main/java/br/com/fleme/ceep/ui/activity/ListaNotasActivity.java
@@ -2,8 +2,16 @@ package br.com.fleme.ceep.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import java.nio.file.Files;
+import java.util.List;
 
 import br.com.fleme.ceep.R;
+import br.com.fleme.ceep.dao.NotaDAO;
+import br.com.fleme.ceep.model.Nota;
+import br.com.fleme.ceep.ui.adapter.ListaNotasAdapter;
 
 public class ListaNotasActivity extends AppCompatActivity {
 
@@ -11,5 +19,15 @@ public class ListaNotasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_notas);
+
+        ListView listaDeNotas = findViewById(R.id.listView);
+
+        NotaDAO dao = new NotaDAO();
+        dao.insere(new Nota("Primeira Nota", "Primeira descrição..."));
+
+        List<Nota> todasNotas = dao.todos();
+
+        listaDeNotas.setAdapter(new ListaNotasAdapter(this, todasNotas));
+
     }
 }
