e32568fd3f6716595b8763211ce12301d6cd9efb
felleme <felleme@prestadores.amil.com.br>
2018-11-23 18:41:54 -0200

    Entendendo melhor os métodos do RecyclerView.Adapter.

--- app/src/main/java/br/com/fleme/ceep/ui/recyclerview/adapter/ListaNotasAdapter.java
+++ app/src/main/java/br/com/fleme/ceep/ui/recyclerview/adapter/ListaNotasAdapter.java
@@ -4,22 +4,42 @@ import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.ViewGroup;
 
+import java.util.List;
+
+import br.com.fleme.ceep.model.Nota;
+
 public class ListaNotasAdapter extends RecyclerView.Adapter {
+
+    private List<Nota> notas;
+
+    public ListaNotasAdapter(List<Nota>notas) {
+        this.notas = notas;
+    }
 
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        //cria as ViewHolder suficientes (containers)
+
         return null;
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
 
+        //pega as ViewHolder criadas e faz o processo de bind das informações
+        //chamado no momento do scroll da tela para realizar o bind dos demais ViewHolder que serão mostrados
+
+        //internamente o RecyclerView ao acabar as ViewHolder criados pelo onCreateViewHolder durante o scroll
+        //ele pega a primeira ou última view que não está mais visivel e faz um processo de reciclagem sendo retirada da memória para
+        //realizar novamente o bind com novas informações do próximo item que irá aparecer
+
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return notas.size();
     }
 
 }
