80d2f6dab8d21e5a3df52cbc8f44b1796883155b
felleme <felleme@prestadores.amil.com.br>
2018-11-23 18:18:16 -0200

    Setando a dependência e criando o adapter para utilizarmos com o RecyclerView.

--- app/src/main/java/br/com/fleme/ceep/ui/activity/ListaNotasActivity.java
+++ app/src/main/java/br/com/fleme/ceep/ui/activity/ListaNotasActivity.java
@@ -1,17 +1,15 @@
 package br.com.fleme.ceep.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ListView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 
-import java.nio.file.Files;
 import java.util.List;
 
 import br.com.fleme.ceep.R;
 import br.com.fleme.ceep.dao.NotaDAO;
 import br.com.fleme.ceep.model.Nota;
-import br.com.fleme.ceep.ui.adapter.ListaNotasAdapter;
+import br.com.fleme.ceep.ui.recyclerview.adapter.ListaNotasAdapter;
 
 public class ListaNotasActivity extends AppCompatActivity {
 
@@ -20,17 +18,25 @@ public class ListaNotasActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_notas);
 
-        ListView listaDeNotas = findViewById(R.id.listView);
+        //+ dependências - Ctrl + Shift + Alt + S (Project Structure)
+        //find file - Ctrl + Shift + N
+
+        //clean imports - Ctrl + Alt + O
+
+        RecyclerView listaDeNotas = findViewById(R.id.lista_notas_recycler_view);
 
         NotaDAO dao = new NotaDAO();
-
         for(int i = 1; i <= 10000; i++) {
             dao.insere(new Nota("Título " + i, "Descrição " + i));
         }
 
         List<Nota> todasNotas = dao.todos();
 
-        listaDeNotas.setAdapter(new ListaNotasAdapter(this, todasNotas));
+        //adapter do list view
+        //listaDeNotas.setAdapter(new ListaNotasAdapter(this, todasNotas));
+
+        //adapter do recycler view
+        listaDeNotas.setAdapter(new ListaNotasAdapter());
 
     }
 }
---
+++ app/src/main/java/br/com/fleme/ceep/ui/recyclerview/adapter/ListaNotasAdapter.java
@@ -0,0 +1,25 @@
+package br.com.fleme.ceep.ui.recyclerview.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class ListaNotasAdapter extends RecyclerView.Adapter {
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+}
