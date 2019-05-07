00992a7c2358517e4fa242cb8591338425d56cb8
felleme <felleme@prestadores.amil.com.br>
2019-05-07 16:46:23 -0300

    create FormularioNotaActivity

---
+++ app/src/main/java/br/com/fleme/ceep/ui/activity/FormularioNotaActivity.java
@@ -0,0 +1,13 @@
+package br.com.fleme.ceep.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FormularioNotaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_nota);
+    }
+}
